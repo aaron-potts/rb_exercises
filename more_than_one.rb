1f45d82bfe40537681e2d3b89bc9e9b63b70e0bf
Aaron <aaronfpotts@gmail.com>
2023-10-02 06:18:09 -0600

    modifying array

---
+++ more_than_one.rb
@@ -0,0 +1,12 @@
+pets = ['cat', 'dog', 'fish', 'lizard']
+
+my_pets = pets[2..3]
+
+puts "I have a pet #{my_pets[0]} and a pet #{my_pets[1]}!"
+
+my_pets.pop
+my_pets << pets[1]
+puts my_pets
+puts my_pets
+
+puts "I have a pet #{my_pets[0]} and a pet #{my_pets[1]}!"

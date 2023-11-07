15e75b40e3aa3becd16dd24c5154d63191b008e8
Aaron <aaronfpotts@gmail.com>
2023-11-07 05:08:48 -0700

    mutating varibles

---
+++ Optional.rb
@@ -0,0 +1,6 @@
+def say(string)
+    puts string
+end
+
+say "What, does this work?"
+say "Nooo way"
---
+++ mutate.rb
@@ -0,0 +1,9 @@
+array = [1, 2, 3]
+
+def mutate(a)
+    a.pop
+end
+
+puts array
+mutate(array)
+puts array
